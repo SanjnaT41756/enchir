f7684d9f9ec2a72fb4aebb2232e9c5e142f17c9f
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2023-08-06 15:00:26 -0700

    create cross circle button, add css maps

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -1,7 +1,23 @@
-function CircleButton() {
-    return(
-      <div></div>
-    );
-  };
-  
-  export default CircleButton;
+import './CircleButton.scss';
+import React from 'react';
+
+function CircleButton (props) { 
+
+  return(
+    <a href={props.link}>
+      <div className={`circle-button-contain container ${props.type}`}>
+        {props.type === 'cross' && (
+          <>
+            <span className="cross-line1"></span>
+            <span className="cross-line2"></span>
+          </>
+        )}
+      </div>
+    </a>
+  );
+};
+
+
+
+
+export default CircleButton;
--- src/pages/Tester.tsx
+++ src/pages/Tester.tsx
@@ -1,10 +1,12 @@
 import Button from "../components/Button";
+import CircleButton from "../components/CircleButton";
 import './Tester.scss';
 
 function Tester() {
   return(
     <div className='tester-page-contain'>
       <Button type='small' link='/home' text='button' />
+      <CircleButton type="cross" link='/home' />
     </div>
   )
 }

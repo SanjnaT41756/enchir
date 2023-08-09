09ae25fcb91ef77cb5edd4f830593b69d4b99cf9
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2023-08-09 14:55:12 -0700

    create Stars rating component

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,7 +1,26 @@
+import './Stars.scss';
+import React, { useState } from 'react';
+import { StarIcon } from '@chakra-ui/icons';
+
 function Stars() {
-  return(
-    <div></div>
+  const [rating, setRating] = useState(0); // Initial rating state
+
+  return (
+    <div className="star-rating">
+      <div className="stars">
+        {[1, 2, 3, 4, 5].map((index) => (
+          <div
+            key={index}
+            className={`star ${index <= rating ? 'filled' : ''}`}
+            onClick={() => setRating(index)}
+          >
+            <StarIcon />
+          </div>
+        ))}
+      </div>
+    </div>
   );
-};
+}
 
 export default Stars;
+

6c340e298d84830b002d4323d1ab8b4b164bb803
Kimberly <kimhnguyeno@g.ucla.edu>
2023-08-21 15:17:39 -0700

    Added back and next buttons and toggle list for tutorial pages.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 import Button from './components/Button';
 import Tutorial_1 from './pages/Tutorial_1';
 import Tutorial_2 from './pages/Tutorial_2';
+import Tutorial_3 from './pages/Tutorial_3';
+
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Tutorial_1/>} />
           <Route path="/T2" element={<Tutorial_2/>} />
+          <Route path="/T3" element={<Tutorial_3/>} />
         </Routes>
       </div>
     </ChakraProvider>
--- src/pages/Tutorial_2.tsx
+++ src/pages/Tutorial_2.tsx
@@ -1,15 +1,37 @@
-import Button from "../components/Button";
-import Header from "../components/Header";
-import enchirNeutral from '../assets/images/enchirNeutral.png';
-
-import './Tutorial_1.scss';
-
+import { useState } from 'react';
+import Button from '../components/Button';
+import Header from '../components/Header';
+import Accordian from '../components/Accordian';
+import './Tutorial_2.scss';
 function Tutorial_2() {
-  return(
-    <div className='page-contain'> 
-      <Button type='default' link='#/' text='back' />
-    </div>
-  )
-}
+  const items = [
+    {
+      name: 'blah blah blah blah 0',
+      content: 'lorem ipsum stuff'
+    },
+    {
+      name: 'blah blah blah blah 1',
+      content: 'lorem ipsum stuff'
+    },
+    {
+      name: 'blah blah blah blah 2',
+      content: 'lorem ipsum stuff'
+    },
+  ]
+    return(
+        <div className='page-contain tut-2'> 
+        <Header className='header' page='welcome to enchir' />
+        <section className='content'>
+          <h3>as we explore the web together, i can help you:</h3>
+          <Accordian items={items}/>
+        </section>
+        <div className='footer-button'>
+          <Button type='default' link='#/' text='back' />
+          <Button type='default' link='#/T3' text='next' />
+        </div>
+  
+      </div>
+    );
+ };
 
 export default Tutorial_2

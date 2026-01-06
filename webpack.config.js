d6e34e274b5664e39f7066df688bfe92e799d77e
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2026-01-06 14:42:36 -0800

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,14 @@ module.exports = {
             {
                 test:/\.scss$/,
                 use: [
-                    "style-loader", "css-loader", "sass-loader"
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass"),
+                        },
+                    }
                 ]
             },
             {
